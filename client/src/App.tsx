b4527e6fb43bb24741a75cd59b8b90af24d73cd0
DarKFlasH07 <kesarwaniaryan76@gmail.com>
2025-04-25 15:32:53 +0530

    added the all files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import Dashboard from './pages/Dashboard'
 import Landing from './pages/Landing'
 import Upload from './pages/Upload'
 import Uploads from './pages/Uploads'
+import ArweaveFiles from './pages/ArweaveFiles'
 import GoogleDrive from './pages/GoogleDrive'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/upload" element={<Upload />} />
           <Route path="/uploads" element={<Uploads />} />
+          <Route path="/arweavefiles" element={<ArweaveFiles />} />
           <Route path="/google-drive" element={<GoogleDrive />} />
         </Routes>
       </BrowserRouter>
